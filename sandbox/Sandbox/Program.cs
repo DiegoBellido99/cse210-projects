a59ffa327cd8a3c6efaa134f4ed9c0b29e421533
DiegoBellido99 <dbellidoochoa@gmail.com>
2024-01-10 22:21:39 -0500

    this a changed

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello diego bellido !");
+        Console.WriteLine("Hello diego bellido this a try !");
     }
 }
