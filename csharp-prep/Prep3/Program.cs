c2d416ecaf492dc383a8fc7e3d95b68d8333aac0
DiegoBellido99 <dbellidoochoa@gmail.com>
2024-01-12 23:16:06 -0500

    I did my third code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the secret number ?");
+        int number = int.Parse(Console.ReadLine());
+        
+        int guess = 0;
+        while ( guess != number)
+        {
+            Console.Write("What do you guess : ");
+            guess= int.Parse(Console.ReadLine());
+
+            if (guess < number)
+            {
+              Console.WriteLine("Higher");
+            }
+            else if (guess > number)
+            {
+              Console.WriteLine("Lower");
+            }
+            else
+            {
+              Console.WriteLine("You guessed it !!");
+            }
+        }
     }
 }
