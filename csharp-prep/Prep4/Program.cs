03afe0f792860c886248709b732f0c76d7ba961a
DiegoBellido99 <dbellidoochoa@gmail.com>
2024-01-13 20:48:00 -0500

    I did my forth code

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Collections.Generic;
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+       List<int> numbers = new List<int>();
+       
+       Console.WriteLine("  Enter a list of numbers, type 0 when finished. ");
+       
+       int content =-1;
+       while (content != 0 )
+       {
+        Console.Write("Enter a number : ");
+        string input = Console.ReadLine();
+        content= int.Parse(input);
+       
+        if (content != 0)
+        {
+            numbers.Add(content);
+        }
+        }
+
+         int  futureValue = 0 ;
+
+         foreach(int value in numbers)
+         {
+            futureValue+= value;
+         }
+         Console.WriteLine($"The total is : {futureValue}");
+
+         float average = ((float)futureValue)/numbers.Count;
+         Console.WriteLine($"The average is {average}");
+
+         int max = numbers[0] ;
+
+         foreach (int values in numbers)
+         {
+            if ( max < values)
+            {
+                max=values;
+            }
+         }
+         Console.WriteLine($"The maximun value is {max}");
+     
+        
     }
 }
