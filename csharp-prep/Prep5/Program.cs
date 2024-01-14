ccc5cbb38e11bd86959b43b1d990da6e834c484d
DiegoBellido99 <dbellidoochoa@gmail.com>
2024-01-13 22:59:40 -0500

    I did my last code

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -3,7 +3,44 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep5 World!");
+    {   
+        DisplayWelcome();
+        string fullName = PromptUserName();
+        int favoriteNumber = PromptUserNumber();
+        int square= SquareNumber(favoriteNumber);
+        DisplayResult(fullName,square);
+        
+
+        static void DisplayResult (string name , int number)
+        {            
+            Console.WriteLine($"{name} , the square of your number is {number}");
+        }
+       
+        static int SquareNumber(int value)
+        {
+           int total = value * value; 
+           return total ;
+        }
+       
+        static int PromptUserNumber()
+        {
+            Console.Write ("Please enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your full name: ");
+            string name = Console.ReadLine();
+            return name;       
+        }
+
+        static void DisplayWelcome ()
+        {
+            Console.WriteLine("Welcome to the Program!!! ");
+            
+        }        
+
     }
 }
